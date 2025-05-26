0bc3010bbea3a0220b93b245c7c477c047ab5fbb
Calvin Rohloff <zickzenniyt@gmail.com>
2025-05-26 08:51:26 +0200

    Add home screen and modify tsconfig

--- src/renderer/app/router.tsx
+++ src/renderer/app/router.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./routes/Home";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <p>Router</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
