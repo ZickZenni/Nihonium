d7a4798b1d0cd659f012c1b249612e9001d85410
Calvin Rohloff <zickzenniyt@gmail.com>
2025-05-26 08:29:30 +0200

    Add & Remove comments

--- src/main/app.ts
+++ src/main/app.ts
@@ -6,9 +6,11 @@ class Application {
   private mainWindow: BrowserWindow | null = null;
 
   constructor() {
-    // Quit when all windows are closed, except on macOS. There, it's common
-    // for applications and their menu bar to stay active until the user quits
-    // explicitly with Cmd + Q.
+    /*
+     * Quit when all windows are closed, except on macOS. There, it's common
+     * for applications and their menu bar to stay active until the user quits
+     * explicitly with Cmd + Q.
+     */
     electronApp.on("window-all-closed", () => {
       if (process.platform !== "darwin") {
         electronApp.quit();
@@ -16,8 +18,10 @@ class Application {
     });
 
     electronApp.on("activate", () => {
-      // On OS X it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
+      /*
+       * On OS X it's common to re-create a window in the app when the
+       * dock icon is clicked and there are no other windows open.
+       */
       if (BrowserWindow.getAllWindows().length === 0) {
         this.createMainWindow();
       }
@@ -25,7 +29,9 @@ class Application {
   }
 
   public async init() {
-    // Handle creating/removing shortcuts on Windows when installing/uninstalling.
+    /*
+     * Handle creating/removing shortcuts on Windows when installing/uninstalling.
+     */
     if (squirrelStartup) {
       electronApp.quit();
       return;
@@ -48,7 +54,9 @@ class Application {
       console.log(`Window created: ${bounds.width}x${bounds.height}`);
     }
 
-    // and load the index.html of the app.
+    /*
+     * Load the index.html of the app.
+     */
     if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
       await mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
     } else {
